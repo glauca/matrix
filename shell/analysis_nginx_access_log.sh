a484f4c3c5e39d291921a89a923b7e3e668339ea
duruo <duruo@126.com>
2017-02-05 10:33:29 +0800

    upgrade 分析 Nginx 资源请求数

--- shell/analysis_nginx_access_log.sh
+++ shell/analysis_nginx_access_log.sh
@@ -2,9 +2,28 @@
 
 if [[ -f $1 ]]; then
 
-    filename=$1
-    echo -e "$filename \n"
+    accessLog=$1
+    echo -e "$accessLog \n"
 
-    tail -10000 $filename | awk '{print $7}' | sort | uniq -c | sort -k1,1nr | head -10
+    # 当前日期
+    currentDate=`date '+%F'`
+    logFile="/tmp/bannersRequest-$currentDate.log"
+
+    echo `date '+%F %R'` >> $logFile
+
+    tail -100000 $accessLog | awk '{print $7"-"int($10/1024)"k"}' | sort | uniq -c | sort -k1,1nr | head -25 >> $logFile
+
+    # 清空 access.log 文件
+    echo '' > $accessLog
+
+    tail -26 $logFile
+
+    # 删除一周前的保存记录
+    aWeekAgoDate=`date +'%F' --date='a week ago'`
+    aWeekAgoFile="/tmp/bannersRequest-$aWeekAgoDate.log"
+
+    if [[ -f $aWeekAgoFile ]]; then
+        rm -f $aWeekAgoFile
+    fi
 
 fi
