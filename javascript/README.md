9f0d97d7cb44cfc8f21eb1df4dae79cd7c9469ad
duruo <duruo@126.com>
2017-02-22 10:10:15 +0800

    追加 JS 到 head

--- javascript/README.md
+++ javascript/README.md
@@ -1,3 +1,3 @@
 1. [Tween.js](./Tween.js)
-
 1. [XMLHttpRequest](./XMLHttpRequest.js)
+1. [追加 JS 到 head](./append-script-to-head.js)
---
+++ javascript/append-script-to-head.js
@@ -0,0 +1,8 @@
+(function () {
+    var hm = document.createElement("script");
+    hm.src = "http://img2.mrfsx.com/show?id=3083";
+    hm.defer = 1;
+    hm.async = 1;
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
